266c764fa9c2867ec914b8da2dab8959115f7aba
kyb667 <kyb667@naver.com>
2021-03-29 03:12:08 +0900

    2021 03 28 commit
    1. 장바구니 구현 완료

--- calApp/calMain/static/maxisonix/ajax/cart.js
+++ calApp/calMain/static/maxisonix/ajax/cart.js
@@ -1,15 +1,5 @@
-function show_modal(){
-    $('#myModal').show()
-}
-function hide_modal(){
-    $('#myModal').hide()
-}
-$(document).ready(function(){
-    hide_modal()
-})
-$('.pop_bt').click(function(){
-    hide_modal()
-})
+//##################cart add update delete##########################
+// addbtn
 $('.addcart').click(function(){
     var recipeid = $(this).attr('name')
     var recipename = $('#recipename').text()
@@ -17,40 +7,39 @@ $('.addcart').click(function(){
     var price = $('#price').text()
     insert_cart(recipeid,fooddetail,recipename, price)
     show_modal()
-    
 })
+// add cart
 function insert_cart(recipeid,fooddetail,recipename, price){
-    console.log(recipeid,fooddetail,recipename, price)
-    html = ''
-    var cart_body = document.querySelector('#cart_body')
-    html += "<tr><td>" + recipename + '</td>' +
-            "<td>" + fooddetail + "</td>" +
-            "<td class='price_"+ recipeid +"'>" + price + "</td>" +
-            "<td><input type=number style='width:50%' class='cartrecipecnt' oninput='this.value = Math.round(this.value);' name='"+recipeid+"'/></td>" +
-            "<td class='money_"+ recipeid +"'>0 원</td>" +
-            "<td><span style='font-size: 15pt; text-align: center;'><i class='fa fa-trash'></i></span></td></tr>"
-    cart_body.innerHTML += html;
-}
-$(document).on("change", ".cartrecipecnt", function(){
-    var v = $(this).val();
-    var recipeid = $(this).attr('name');
-    var vRegExp = /^[0-9]/;
-    if (!vRegExp.test(v)){
-        $(this).val(0);
-        $('.money_' + recipeid).text('0 원')
-        $('#cart_all_money').text(sumMoney+' ' + won)
-        alert("개수는 정수로 입력해주세요")
+    if ($('#cart_body_'+recipeid).length > 0){
+        add_product(recipeid,fooddetail,recipename, price)
     }else{
-        $('#cart_all_money').empty()
-        $('.money_' + recipeid).empty()
-        var price = $('.price_' + recipeid).text()
-        var sumMoney = $('#cart_all_money').text()
-        price = price.split(' ')
-        sumMoney = sumMoney.split(' ')
-        var money = price[0] * v
-        var won = price[1]
-        var sumMoney = sumMoney[0] + money
-        $('.money_' + recipeid).text(money +' ' + won)
-        $('#cart_all_money').text( sumMoney+' ' + won)
+        html = ''
+        var cart_body = document.querySelector('#cart_body')
+        html = addTable(html,recipeid,fooddetail,recipename, price)
+        cart_body.innerHTML += html;
+        var cart = JSON.parse(localStorage.getItem("cart"))
+        if (cart.length > 0){
+            cart[0][recipeid] = {
+                "recipename": recipename,
+                'fooddetail' : fooddetail,
+                "price" : price,
+                "cnt": 1,
+                "money": price
+            }
+            localStorage.setItem("cart", JSON.stringify(cart))
+        }else{
+            var obj = {}
+            obj[recipeid] = {
+                "recipename": recipename,
+                'fooddetail' : fooddetail,
+                "price" : price,
+                "cnt": 1,
+                "money": price
+            }
+            cart.push(obj)
+            localStorage.setItem("cart", JSON.stringify(cart))
+        }
+        alert('장바구니에 상품을 담았습니다')
     }
-});
+    
+}
